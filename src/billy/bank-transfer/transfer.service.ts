2bb4731170bdcacc20fcdc98bf88a32f7a993b05
gooddeedstech <samuel.osarieme@gooddeedstech.net>
2025-12-02 09:22:48 +0100

    v2

--- src/billy/bank-transfer/transfer.service.ts
+++ src/billy/bank-transfer/transfer.service.ts
@@ -82,15 +82,7 @@ async executeTransfer(
   // 🔥 STOP if insufficient balance
   try {
     this.ensureSufficientBalance(user, payload.amount);
-  } catch (err) {
-    await this.cache.delete(`tx:${phone}`);
-    return this.whatsappApi.sendText(
-      phone,
-      `❗ Insufficient wallet balance.\nYour current balance is ₦${Number(user.balance || 0).toLocaleString()}`
-    );
-  }
-
-  const request: RubiesTransferDto = {
+    const request: RubiesTransferDto = {
     amount: payload.amount,
     creditAccountNumber: payload.accountNumber,
     creditAccountName: payload.accountName,
@@ -109,6 +101,16 @@ async executeTransfer(
   await this.userService.update(user.id, user);
 
   return tx;
+  
+  } catch (err) {
+    await this.cache.delete(`tx:${phone}`);
+    return this.whatsappApi.sendText(
+      phone,
+      `❗ Insufficient wallet balance.\nYour current balance is ₦${Number(user.balance || 0).toLocaleString()}`
+    );
+  }
+
+  
 }
 
   // ---------------- BENEFICIARY ----------------
